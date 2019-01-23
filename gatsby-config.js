b10a14897624167532da2f2cdcff80984d83ff33
Jason  Elgin <jason@jasons-mbp.attlocal.net>
2019-01-22 22:50:42 -0500

    updating gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,34 @@
 module.exports = {
   siteMetadata: {
-    title: 'Hello. My name is Jason Elgin',
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet', 
-    'gatsby-plugin-sass',
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      }
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
     },
-    'gatsby-transformer-remark',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
   ],
 }
