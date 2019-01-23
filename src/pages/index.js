06f7616c7ab023482a7eeb6fa542168f31fa0309
Jason  Elgin <jason@jasons-mbp.attlocal.net>
2019-01-22 23:22:12 -0500

    adding scss

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,16 @@ import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title="Home" keywords={[`Jason Elgin`, `ux engineer`, `developer`]} />
+    <div className="main">
+      <h2>hello</h2>
+      <p> <a href="https://twitter.com/jrelgin">@jrelgin</a></p>
+      <div className="astro" style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>    
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+   
   </Layout>
 )
 
