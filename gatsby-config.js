acbe181b5770a725a43fb0f6bada99de4c12ca10
Jason  Elgin <jason@Jasons-MBP.local>
2019-02-03 16:09:44 -0500

    scraping instagram for post

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,19 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       }
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: 'posts'
+      }
+    },
+    {
+      resolve: `gatsby-source-instagram`,
+      options: {
+        username: `jrelgin`
+      }
+    },    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
