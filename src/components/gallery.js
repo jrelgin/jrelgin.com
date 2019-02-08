e2c5509a0b3c690607ffc734bbca52d4330cd703
Jason  Elgin <jason@jasons-mbp.attlocal.net>
2019-02-07 20:36:22 -0500

    clean up query in gallery component

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,37 +1,40 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
-const Gallery = () => (
-  <StaticQuery
-    query={graphql`
-      query instaQuery {
-        allInstaNode {
-          edges {
-            node {
-              caption
-              localFile {
-                childImageSharp {
-                  fixed(width: 600, height: 600) {
-                    src
-                  }
-                }
+const SCRAP_INSTA_QUERY = graphql`
+  query instaQuery {
+    allInstaNode {
+      edges {
+        node {
+          id
+          caption
+          localFile {
+            childImageSharp {
+              fixed(width: 600, height: 600) {
+                src
               }
             }
           }
         }
       }
-    `}
+    }
+  }
+`
+const Gallery = () => (
+  <StaticQuery
+    query={SCRAP_INSTA_QUERY}
     render={({allInstaNode}) => (
       <>
         <section className="gallery">
           <h3>On Instagram</h3>
-          {allInstaNode.edges.map(edge => (
-            <li>
-              <img src={edge.node.localFile.childImageSharp.fixed.src} alt=""/>
-              <span className="caption">{edge.node.caption}</span>
-            </li>
-          ))}
+          <ul>
+            {allInstaNode.edges.map(edge => (
+              <li key={edge.node.id}>
+                <img src={edge.node.localFile.childImageSharp.fixed.src} alt=""/>
+                <span className="caption">{edge.node.caption}</span>
+              </li>
+            ))}
+          </ul>
         </section>
       </>
     )}
